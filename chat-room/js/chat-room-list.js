6f8c073594653197a43166a9d6a62dc0efb94936
AleksandrinaKovachka <aleksandrina.kowachka@gmail.com>
2022-02-13 23:43:11 +0200

    Clean same piece of code in css

--- chat-room/js/chat-room-list.js
+++ chat-room/js/chat-room-list.js
@@ -146,27 +146,3 @@ onChildChanged(dataRefRooms, (data) => {
         addRoomBtn.disabled = true;
     })
 })
-
-// window.onload = event => {
-//     let roomlist = document.getElementById("chat-rooms-list");
-
-//     rooms.forEach((item, index) => {
-//         let li = document.createElement("li");
-//         li.innerHTML = `
-//         <div id="chat-room">
-//             <p id="room-name">${item}</p>
-//             <button id="add-room-${index}" class="add-room-class">Add</button>
-//         </div>`;
-//         roomlist.appendChild(li);
-
-//         let addRoomBtn = document.getElementById(`add-room-${index}`);
-//         addRoomBtn.addEventListener("click", () => {
-//             //added to rooms of the user - get the name of room
-//             console.log(item);
-//             //remove from array
-//             console.log("added");
-//             addRoomBtn.textContent = "Added";
-//             addRoomBtn.disabled = true;
-//         })
-//     })
-// }
